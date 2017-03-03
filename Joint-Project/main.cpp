f8491856ed0a68a73cedb244e06636af98ac5bda
(Student) - Aaron Curry <C00202505@ITCARLOW.IE>
2017-03-03 12:25:05 +0000

    Deleted branch files

--- Joint-Project/main.cpp
+++ Joint-Project/main.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 
 int main()
-<<<<<<< HEAD
 {
 
 	std::cout << "Team leader says hello (edited)" << std::endl;
 	std::cout << "Aaron says hello" << std::endl;
 
-=======
 {	
 	std::cout << "Team leader says hello (edited)" << std::endl;
 	std::cout << "Jake says hello" << std::endl;
->>>>>>> c9a49faec5741488d85e73685855306f4d02d6a0
 
 	system("PAUSE");
 }
